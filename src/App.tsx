ce216380f350f90ec96b7df936d7d1fb2d2f9a31
Guilherme Girardi <guilherme@ardanis.com>
2024-01-05 14:53:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-[425px] max-w-[425px] flex-col bg-gradient-to-r from-blue-900 to-gray-700 p-8">
+      <p className="text-lg">Ocean Friends Test</p>
+    </div>
+  )
+}
+
+export default App
