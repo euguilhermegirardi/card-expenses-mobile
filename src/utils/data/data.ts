499145149a04e1ac82f9af5837ee1badfb45934a
Guilherme Girardi <guilherme@ardanis.com>
2024-01-05 17:32:50 -0300

    create fake data

---
+++ src/utils/data/data.ts
@@ -0,0 +1,84 @@
+export const data = [
+  {
+    catalogue: [
+      {
+        title: 'Mobile',
+        description: 'The debit is ',
+        value: '$34.00',
+        pay: 'active',
+        type: 'mobile',
+        id: 1,
+      },
+      {
+        title: 'Internet and TV',
+        description: 'The debit is ',
+        value: '$21.00',
+        pay: 'active',
+        type: 'internet',
+        id: 2,
+      },
+      {
+        title: 'Traffic fines',
+        description: 'The debit is ',
+        value: '$1,221.00',
+        pay: 'active',
+        type: 'traffic',
+        id: 3,
+      },
+      {
+        title: 'Housing services',
+        description: 'The debit is ',
+        value: '$0.00',
+        pay: 'inactive',
+        type: 'housing',
+        id: 5,
+      },
+      {
+        title: 'Utility payment',
+        description: 'The debit is ',
+        value: '$442.00',
+        pay: 'active',
+        type: 'utility',
+        id: 6,
+      },
+    ],
+    history: [
+      {
+        title: 'Card to card',
+        description: 'Maria',
+        value: '+ $143.00',
+        id: 1,
+      },
+      {
+        title: 'Apple Music',
+        description: 'Online',
+        value: '- $467.00',
+        id: 2,
+      },
+      {
+        title: 'Uber',
+        description: 'Service',
+        value: '- $467.00',
+        id: 3,
+      },
+      {
+        title: 'Uber',
+        description: 'Service',
+        value: '- $43.00',
+        id: 4,
+      },
+      {
+        title: 'Card to card',
+        description: 'Service',
+        value: '- $2467.00',
+        id: 5,
+      },
+      {
+        title: 'Card to card',
+        description: 'Maria',
+        value: '+ $1443.00',
+        id: 6,
+      },
+    ],
+  },
+]
